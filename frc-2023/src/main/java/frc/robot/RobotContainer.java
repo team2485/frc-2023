0c64c59504d35daf992d42714e9c4610aa8bdb2d
Andrew Zhou <azhou2023@francisparker.org>
2023-01-17 19:17:46 -0800

    init

---
+++ frc-2023/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,79 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.WarlordsLib.WL_CommandXboxController;
+import frc.robot.Constants.OIConstants;
+import frc.robot.commands.DriveWithController;
+import frc.robot.subsystems.drive.Drivetrain;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  private final WL_CommandXboxController m_driver = new WL_CommandXboxController(OIConstants.kDriverPort);
+
+  private final WL_CommandXboxController m_operator  =
+      new WL_CommandXboxController(OIConstants.kOperatorPort);
+
+  public final Drivetrain m_drivetrain = new Drivetrain();
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+
+    configureDrivetrainCommands();
+
+    
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+
+  private void configureDrivetrainCommands(){
+
+    m_drivetrain.setDefaultCommand(
+        new DriveWithController(m_drivetrain, 
+                                ()->m_driver.getLeftY(), 
+                                ()->m_driver.getLeftX(), 
+                                ()->m_driver.getRightX(), 
+                                ()->{return m_driver.rightBumper().getAsBoolean();})
+    );
+
+    m_driver.x().onTrue(new InstantCommand(()->m_drivetrain.zeroGyro()));
+  }
+
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return null;
+  }
+}
---
+++ frc-2023/src/main/java/frc/util/SwerveModuleConstants.java
@@ -0,0 +1,27 @@
+package frc.util;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public class SwerveModuleConstants {
+    public final int driveMotorID;
+    public final int angleMotorID;
+    public final int cancoderID;
+    public final Rotation2d angleOffset;
+    public final boolean isInverted;
+
+
+    /**
+     * Swerve Module Constants to be used when creating swerve modules.
+     * @param driveMotorID
+     * @param angleMotorID
+     * @param canCoderID
+     * @param angleOffset
+     */
+    public SwerveModuleConstants(int driveMotorID, int angleMotorID, int canCoderID, Rotation2d angleOffset, boolean isInverted) {
+        this.driveMotorID = driveMotorID;
+        this.angleMotorID = angleMotorID;
+        this.cancoderID = canCoderID;
+        this.angleOffset = angleOffset;
+        this.isInverted = isInverted;
+    }
+}
