1b28de52b5499ba85faace838fee013eae924b75
Jack Douglas <jackdouglas2@me.com>
2023-01-28 10:38:36 -0800

    init commit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,7 +7,11 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import frc.WarlordsLib.sendableRichness.SR_TrapezoidProfile;
@@ -15,11 +19,15 @@ import frc.util.COTSFalconSwerveConstants;
 import frc.util.SwerveModuleConstants;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -56,143 +64,167 @@ public final class Constants {
 
   public static final double stickDeadband = 0.1;
 
-    public static final class Swerve {
-        public static final int pigeonID = 9;
-        public static final boolean invertGyro = false; // Always ensure Gyro is CCW+ CW-
+  public static final class Swerve {
+    public static final int pigeonID = 9;
+    public static final boolean invertGyro = false; // Always ensure Gyro is CCW+ CW-
 
-        public static final COTSFalconSwerveConstants chosenModule =  //TODO: This must be tuned to specific robot
-            COTSFalconSwerveConstants.SDSMK4(COTSFalconSwerveConstants.driveGearRatios.SDSMK4_L2);
+    public static final COTSFalconSwerveConstants chosenModule = // TODO: This must be tuned to specific robot
+        COTSFalconSwerveConstants.SDSMK4(COTSFalconSwerveConstants.driveGearRatios.SDSMK4_L2);
 
-        /* Drivetrain Constants */
-        public static final double trackWidth = 0.4445;
-        public static final double wheelBase = 0.6477;
-        public static final double wheelCircumference = chosenModule.wheelCircumference;
+    /* Drivetrain Constants */
+    public static final double trackWidth = 0.4445;
+    public static final double wheelBase = 0.6477;
+    public static final double wheelCircumference = chosenModule.wheelCircumference;
 
-        /* Swerve Kinematics 
-         * No need to ever change this unless you are not doing a traditional rectangular/square 4 module swerve */
-         public static final SwerveDriveKinematics swerveKinematics = new SwerveDriveKinematics(
-            new Translation2d(wheelBase / 2.0, trackWidth / 2.0),
-            new Translation2d(wheelBase / 2.0, -trackWidth / 2.0),
-            new Translation2d(-wheelBase / 2.0, trackWidth / 2.0),
-            new Translation2d(-wheelBase / 2.0, -trackWidth / 2.0));
+    /*
+     * Swerve Kinematics
+     * No need to ever change this unless you are not doing a traditional
+     * rectangular/square 4 module swerve
+     */
+    public static final SwerveDriveKinematics swerveKinematics = new SwerveDriveKinematics(
+        new Translation2d(wheelBase / 2.0, trackWidth / 2.0),
+        new Translation2d(wheelBase / 2.0, -trackWidth / 2.0),
+        new Translation2d(-wheelBase / 2.0, trackWidth / 2.0),
+        new Translation2d(-wheelBase / 2.0, -trackWidth / 2.0));
 
-        /* Module Gear Ratios */
-        public static final double driveGearRatio = chosenModule.driveGearRatio;
-        public static final double angleGearRatio = chosenModule.angleGearRatio;
+    /* Module Gear Ratios */
+    public static final double driveGearRatio = chosenModule.driveGearRatio;
+    public static final double angleGearRatio = chosenModule.angleGearRatio;
 
-        /* Motor Inverts */
-        public static final boolean angleMotorInvert = chosenModule.angleMotorInvert;
-        public static final boolean driveMotorInvert = chosenModule.driveMotorInvert;
+    /* Motor Inverts */
+    public static final boolean angleMotorInvert = chosenModule.angleMotorInvert;
+    public static final boolean driveMotorInvert = chosenModule.driveMotorInvert;
 
-        /* Angle Encoder Invert */
-        public static final boolean canCoderInvert = chosenModule.canCoderInvert;
+    /* Angle Encoder Invert */
+    public static final boolean canCoderInvert = chosenModule.canCoderInvert;
 
-        /* Swerve Current Limiting */
-        public static final int angleContinuousCurrentLimit = 25;
-        public static final int anglePeakCurrentLimit = 40;
-        public static final double anglePeakCurrentDuration = 0.1;
-        public static final boolean angleEnableCurrentLimit = true;
+    /* Swerve Current Limiting */
+    public static final int angleContinuousCurrentLimit = 25;
+    public static final int anglePeakCurrentLimit = 40;
+    public static final double anglePeakCurrentDuration = 0.1;
+    public static final boolean angleEnableCurrentLimit = true;
 
-        public static final int driveContinuousCurrentLimit = 35;
-        public static final int drivePeakCurrentLimit = 60;
-        public static final double drivePeakCurrentDuration = 0.1;
-        public static final boolean driveEnableCurrentLimit = true;
+    public static final int driveContinuousCurrentLimit = 35;
+    public static final int drivePeakCurrentLimit = 60;
+    public static final double drivePeakCurrentDuration = 0.1;
+    public static final boolean driveEnableCurrentLimit = true;
 
-        /* These values are used by the drive falcon to ramp in open loop and closed loop driving.
-         * We found a small open loop ramp (0.25) helps with tread wear, tipping, etc */
-        public static final double openLoopRamp = 0.25;
-        public static final double closedLoopRamp = 0.0;
+    /*
+     * These values are used by the drive falcon to ramp in open loop and closed
+     * loop driving.
+     * We found a small open loop ramp (0.25) helps with tread wear, tipping, etc
+     */
+    public static final double openLoopRamp = 0.25;
+    public static final double closedLoopRamp = 0.0;
 
-        /* Angle Motor PID Values */
-        public static final double angleKP = chosenModule.angleKP;
-        public static final double angleKI = chosenModule.angleKI;
-        public static final double angleKD = chosenModule.angleKD;
-        public static final double angleKF = chosenModule.angleKF;
+    /* Angle Motor PID Values */
+    public static final double angleKP = chosenModule.angleKP;
+    public static final double angleKI = chosenModule.angleKI;
+    public static final double angleKD = chosenModule.angleKD;
+    public static final double angleKF = chosenModule.angleKF;
 
-        /* Drive Motor PID Values */
-        public static final double driveKP = 0.1; 
-        public static final double driveKI = 0.0;
-        public static final double driveKD = 0.0;
-        public static final double driveKF = 0.0;
+    /* Drive Motor PID Values */
+    public static final double driveKP = 0.1;
+    public static final double driveKI = 0.0;
+    public static final double driveKD = 0.0;
+    public static final double driveKF = 0.0;
 
-        /* Drive Motor Characterization Values 
-         * Divide SYSID values by 12 to convert from volts to percent output for CTRE */
-        public static final double driveKS = (0.32 / 12); //TODO: This must be tuned to specific robot
-        public static final double driveKV = (1.51 / 12);
-        public static final double driveKA = (0.27 / 12);
+    /*
+     * Drive Motor Characterization Values
+     * Divide SYSID values by 12 to convert from volts to percent output for CTRE
+     */
+    public static final double driveKS = (0.32 / 12); // TODO: This must be tuned to specific robot
+    public static final double driveKV = (1.51 / 12);
+    public static final double driveKA = (0.27 / 12);
 
-        /* Swerve Profiling Values */
-        /** Meters per Second */
-        public static final double maxSpeed = 3; //TODO: This must be tuned to specific robot
-        /** Radians per Second */
-        public static final double maxAngularVelocity = 4; //TODO: This must be tuned to specific robot
+    /* Swerve Profiling Values */
+    /** Meters per Second */
+    public static final double maxSpeed = 3; // TODO: This must be tuned to specific robot
+    /** Radians per Second */
+    public static final double maxAngularVelocity = 4; // TODO: This must be tuned to specific robot
 
-        /* Neutral Modes */
-        public static final NeutralMode angleNeutralMode = NeutralMode.Brake;
-        public static final NeutralMode driveNeutralMode = NeutralMode.Brake;
+    /* Neutral Modes */
+    public static final NeutralMode angleNeutralMode = NeutralMode.Brake;
+    public static final NeutralMode driveNeutralMode = NeutralMode.Brake;
 
-        /* Module Specific Constants */
-        /* Front Left Module - Module 0 */
-        public static final class Mod0 { //TODO: This must be tuned to specific robot
-          public static final int driveMotorID = 3;
-          public static final int angleMotorID = 4;
-          public static final int canCoderID = 12;
-          public static final Rotation2d angleOffset = Rotation2d.fromDegrees(-20);   
-          public static final SwerveModuleConstants constants = 
-              new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset, false);
-      }
-
-      /* Front Right Module - Module 1 */
-      public static final class Mod1 { //TODO: This must be tuned to specific robot
-          public static final int driveMotorID = 1; 
-          public static final int angleMotorID = 2;
-          public static final int canCoderID = 11;
-          public static final Rotation2d angleOffset = Rotation2d.fromDegrees(-134.485901);
-          public static final SwerveModuleConstants constants = 
-              new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset, false);
-      }
-      
-      /* Back Left Module - Module 2 */
-      public static final class Mod2 { //TODO: This must be tuned to specific robot
-          public static final int driveMotorID = 5;
-          public static final int angleMotorID = 6;
-          public static final int canCoderID = 10;
-          public static final Rotation2d angleOffset = Rotation2d.fromDegrees(-36.325378);
-          public static final SwerveModuleConstants constants =
-              new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset, true);
-      }
-
-      /* Back Right Module - Module 3 */
-      public static final class Mod3 { //TODO: This must be tuned to specific robot
-          public static final int driveMotorID = 7;
-          public static final int angleMotorID = 8;
-          public static final int canCoderID = 13;
-          public static final Rotation2d angleOffset = Rotation2d.fromDegrees(-53.407288);
-          public static final SwerveModuleConstants constants = 
-              new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset, true);
-      }
+    /* Module Specific Constants */
+    /* Front Left Module - Module 0 */
+    public static final class Mod0 { // TODO: This must be tuned to specific robot
+      public static final int driveMotorID = 3;
+      public static final int angleMotorID = 4;
+      public static final int canCoderID = 12;
+      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(-20);
+      public static final SwerveModuleConstants constants = new SwerveModuleConstants(driveMotorID, angleMotorID,
+          canCoderID, angleOffset, false);
     }
 
-    public static final class AutoConstants { //TODO: The below constants are used in the example auto, and must be tuned to specific robot
-        public static final double kMaxSpeedMetersPerSecond = 3;
-        public static final double kMaxAccelerationMetersPerSecondSquared = 3;
-        public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
-        public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
-    
-        public static final double kPAutoXController = 1;
-        public static final double kIAutoXController = 0.05;
-        public static final double kDAutoXController = 0.2;
-        public static final double kPAutoYController = 1;
-        public static final double kIAutoYController = 0.05;
-        public static final double kDAutoYController = 0.2;
-
-        public static final double kPAutoThetaController = 1;
-        public static final double kIAutoThetaController = 0.1;
-        public static final double kDAutoThetaController = 0.1;
-    
-        /* Constraint for the motion profilied robot angle controller */
-        public static final SR_TrapezoidProfile.Constraints kThetaControllerConstraints =
-            new SR_TrapezoidProfile.Constraints(
-                kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
+    /* Front Right Module - Module 1 */
+    public static final class Mod1 { // TODO: This must be tuned to specific robot
+      public static final int driveMotorID = 1;
+      public static final int angleMotorID = 2;
+      public static final int canCoderID = 11;
+      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(-134.485901);
+      public static final SwerveModuleConstants constants = new SwerveModuleConstants(driveMotorID, angleMotorID,
+          canCoderID, angleOffset, false);
     }
+
+    /* Back Left Module - Module 2 */
+    public static final class Mod2 { // TODO: This must be tuned to specific robot
+      public static final int driveMotorID = 5;
+      public static final int angleMotorID = 6;
+      public static final int canCoderID = 10;
+      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(-36.325378);
+      public static final SwerveModuleConstants constants = new SwerveModuleConstants(driveMotorID, angleMotorID,
+          canCoderID, angleOffset, true);
+    }
+
+    /* Back Right Module - Module 3 */
+    public static final class Mod3 { // TODO: This must be tuned to specific robot
+      public static final int driveMotorID = 7;
+      public static final int angleMotorID = 8;
+      public static final int canCoderID = 13;
+      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(-53.407288);
+      public static final SwerveModuleConstants constants = new SwerveModuleConstants(driveMotorID, angleMotorID,
+          canCoderID, angleOffset, true);
+    }
+  }
+
+  public static final class AutoConstants { // TODO: The below constants are used in the example auto, and must be tuned
+                                            // to specific robot
+    public static final double kMaxSpeedMetersPerSecond = 3;
+    public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+    public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
+    public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
+
+    public static final double kPAutoXController = 1;
+    public static final double kIAutoXController = 0.05;
+    public static final double kDAutoXController = 0.2;
+    public static final double kPAutoYController = 1;
+    public static final double kIAutoYController = 0.05;
+    public static final double kDAutoYController = 0.2;
+
+    public static final double kPAutoThetaController = 1;
+    public static final double kIAutoThetaController = 0.1;
+    public static final double kDAutoThetaController = 0.1;
+
+    /* Constraint for the motion profilied robot angle controller */
+    public static final SR_TrapezoidProfile.Constraints kThetaControllerConstraints = new SR_TrapezoidProfile.Constraints(
+        kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
+  }
+
+  public static final class Vision {
+    public static final String kCameraName = "photonvision";
+
+    public static final TrapezoidProfile.Constraints kXConstraints = new TrapezoidProfile.Constraints(2, 2);
+    public static final TrapezoidProfile.Constraints kYConstraints = new TrapezoidProfile.Constraints(2, 2);
+    public static final TrapezoidProfile.Constraints kOmegaConstraints = new TrapezoidProfile.Constraints(8, 8);
+
+    // TODO: ensure validity of measurements
+    public static final Transform3d kCameraToRobot = new Transform3d(new Translation3d(-0.42, 0, -0.97),
+        new Rotation3d());
+
+    public static final int kTagOfInterest = 1;
+    public static final Transform2d kTagToGoal = new Transform2d(new Translation2d(1, 0),
+        Rotation2d.fromDegrees(180.0));
+  }
 }
---
+++ src/main/java/frc/util/vision/PoseEstimation.java
@@ -0,0 +1,144 @@
+package frc.util.vision;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import org.photonvision.PhotonCamera;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.apriltag.AprilTagFieldLayout.OriginPosition;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.Swerve;
+import frc.robot.Constants.Vision;
+import frc.robot.subsystems.drive.Drivetrain;
+
+public class PoseEstimation extends SubsystemBase {
+  private final PhotonCamera m_camera;
+  private final Drivetrain m_drivetrain;
+  private final AprilTagFieldLayout aprilTagFieldLayout;
+
+  // Kalman Filter Configuration. These can be "tuned-to-taste" based on how much
+  // you trust your various sensors. Smaller numbers will cause the filter to
+  // "trust" the estimate from that particular component more than the others.
+  // This in turn means the particualr component will have a stronger influence
+  // on the final pose estimate.
+
+  /**
+   * Standard deviations of model states. Increase these numbers to trust your
+   * model's state estimates less. This
+   * matrix is in the form [x, y, theta]ᵀ, with units in meters and radians, then
+   * meters.
+   */
+  private static final Matrix<N3, N1> stateStdDevs = VecBuilder.fill(0.1, 0.1, Units.degreesToRadians(5));
+
+  /**
+   * Standard deviations of the vision measurements. Increase these numbers to
+   * trust global measurements from vision
+   * less. This matrix is in the form [x, y, theta]ᵀ, with units in meters and
+   * radians.
+   */
+  private static final Matrix<N3, N1> visionMeasurementStdDevs = VecBuilder.fill(0.1, 0.1, Units.degreesToRadians(5));
+
+  private final SwerveDrivePoseEstimator m_poseEstimator;
+
+  private final Field2d field2d = new Field2d();
+
+  private double previousPipelineTimestamp = 0;
+
+  public PoseEstimation(PhotonCamera camera, Drivetrain drivetrain) {
+    this.m_camera = camera;
+    this.m_drivetrain = drivetrain;
+    AprilTagFieldLayout layout;
+
+    try {
+      layout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2023ChargedUp.m_resourceFile);
+      var alliance = DriverStation.getAlliance();
+      layout.setOrigin(alliance == Alliance.Blue ? OriginPosition.kBlueAllianceWallRightSide
+          : OriginPosition.kRedAllianceWallRightSide);
+    } catch (IOException e) {
+      DriverStation.reportError("Failed to load AprilTagFieldLayout", e.getStackTrace());
+      layout = null;
+    }
+
+    this.aprilTagFieldLayout = layout;
+
+    ShuffleboardTab tab = Shuffleboard.getTab("Vision");
+
+    m_poseEstimator = new SwerveDrivePoseEstimator(Swerve.swerveKinematics, m_drivetrain.getYaw(),
+        m_drivetrain.getModulePositions(), new Pose2d(), stateStdDevs, visionMeasurementStdDevs);
+
+    tab.addString("Pose", this::getFormattedPose).withPosition(0, 0).withSize(2, 0);
+    tab.add("Field", field2d).withPosition(2, 0).withSize(6, 4);
+  }
+
+  @Override
+  public void periodic() {
+    // update pose estimator with visible targets
+    var result = m_camera.getLatestResult();
+    var resultTimestamp = result.getTimestampSeconds();
+    if (resultTimestamp != previousPipelineTimestamp && result.hasTargets()) {
+      previousPipelineTimestamp = resultTimestamp;
+      var target = result.getBestTarget();
+      var fiducialId = target.getFiducialId();
+
+      // get tab pose from field layout
+      Optional<Pose3d> tagPose = aprilTagFieldLayout == null ? Optional.empty()
+          : aprilTagFieldLayout.getTagPose(fiducialId);
+      if (target.getPoseAmbiguity() <= .2 && fiducialId >= 0 && tagPose.isPresent()) {
+        var targetPose = tagPose.get();
+        Transform3d camToTarget = target.getBestCameraToTarget();
+        Pose3d camPose = targetPose.transformBy(camToTarget.inverse());
+
+        var visionMeasurement = camPose.transformBy(Vision.kCameraToRobot);
+        m_poseEstimator.addVisionMeasurement(visionMeasurement.toPose2d(), resultTimestamp);
+      }
+    }
+
+    m_poseEstimator.update(m_drivetrain.getYaw(), m_drivetrain.getModulePositions());
+    field2d.setRobotPose(getCurrentPose());
+  }
+
+  private String getFormattedPose() {
+    var pose = getCurrentPose();
+    return String.format("(%.2f, %.2f) %.2f degrees", pose.getX(), pose.getY(), pose.getRotation().getDegrees());
+  }
+
+  public Pose2d getCurrentPose() {
+    return m_poseEstimator.getEstimatedPosition();
+  }
+
+  /**
+   * Resets the current pose to the specified pose. This should ONLY be called
+   * when the robot's position on the field is known, like at the beginning of
+   * a match.
+   * 
+   * @param newPose new pose
+   */
+  public void setCurrentPose(Pose2d newPose) {
+    m_poseEstimator.resetPosition(m_drivetrain.getYaw(), m_drivetrain.getModulePositions(), newPose);
+  }
+
+  /**
+   * Resets the position on the field to 0,0 0-degrees, with forward being
+   * downfield. This resets
+   * what "forward" is for field oriented driving.
+   */
+  public void resetFieldPosition() {
+    setCurrentPose(new Pose2d());
+  }
+}
