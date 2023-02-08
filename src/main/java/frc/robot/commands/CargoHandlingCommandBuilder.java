387fc5f42d398ab41c59c4d052c1ea4e4b12f3ad
Nicholas Smith <57782261+nsmith2023@users.noreply.github.com>
2023-02-07 19:41:49 -0800

    added commands fr

---
+++ src/main/java/frc/robot/commands/CargoHandlingCommandBuilder.java
@@ -0,0 +1,28 @@
+package frc.robot.commands;
+
+import static frc.robot.Constants.MagazineConstants.*;
+
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.subsystems.drive.Magazine;
+import frc.robot.subsystems.drive.Drivetrain;
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+public class CargoHandlingCommandBuilder {
+
+    public static Command setWristBottom(Wrist wrist) {
+        return new InstantCommand(() -> wrist.setAngleRadians(kWristBottomPositionRadians));
+      }
+
+      public static Command setWristMP(Wrist wrist) {
+        return new InstantCommand(() -> wrist.setAngleRadians((kWristBottomPositionRadians+kWristTopPositionRadians)/2));
+      }
+  
+      
+}
