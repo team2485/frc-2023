db9e3777a8d7c0fbc926615245657a38f52145fc
Jack Douglas <jackdouglas2@me.com>
2023-02-18 12:56:44 -0800

    fix magazine subsystem

--- src/main/java/frc/robot/commands/CargoHandlingCommandBuilder.java
+++ src/main/java/frc/robot/commands/CargoHandlingCommandBuilder.java
@@ -1,29 +1,17 @@
 package frc.robot.commands;
 
-import static frc.robot.Constants.MagazineConstants.*;
-
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.StartEndCommand;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.subsystems.drive.Magazine;
-import frc.robot.subsystems.drive.magazine;
-import frc.robot.subsystems.drive.Drivetrain;
-import java.util.function.BooleanSupplier;
-import java.util.function.DoubleSupplier;
+import frc.robot.subsystems.Magazine;
 
 public class CargoHandlingCommandBuilder {
 
-    public static Command magazineOff(Magazine magazine) {
-        return new InstantCommand(() -> magazine.setVelocityRotationsPerSecond(0));
-      }
+  public static Command magazineOff(Magazine magazine) {
+    return new InstantCommand(() -> magazine.setVelocityRotationsPerSecond(0));
+  }
 
-      public static Command magazineOn(magazine magazine) {
-        return new InstantCommand(() -> magazine.setVelocityRotationsPerSecond(5));
-      }
-  
-      
+  public static Command magazineOn(Magazine magazine) {
+    return new InstantCommand(() -> magazine.setVelocityRotationsPerSecond(5));
+  }
+
 }
